5eda9bf208db97c763dabf6d429a79bb00976cfc
Nick Singh <singhharsukh@gmail.com>
2016-09-10 13:48:28 -0400

    current changes

--- app/src/main/java/com/harsukh/testapp/VoiceRecognition.java
+++ app/src/main/java/com/harsukh/testapp/VoiceRecognition.java
@@ -3,6 +3,7 @@ package com.harsukh.testapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 import android.speech.RecognitionListener;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
@@ -18,9 +19,22 @@ public class VoiceRecognition implements RecognitionListener {
     private static final String TAG = "VoiceRec";
     private static final CharSequence initString = "Okay On Star";
 
-    public VoiceRecognition(Context context) {
+    private static VoiceRecognition VR = null;
+
+    private VoiceRecognition(Context context) {
+        Debug.waitForDebugger();
+
         Log.d(TAG, "listening");
         this.context = context;
+    }
+
+    public static VoiceRecognition getInstance(Context context) {
+        if (VR == null) {
+            VR = new VoiceRecognition(context);
+            return VR;
+        } else {
+            return VR;
+        }
     }
 
     @DebugLog
